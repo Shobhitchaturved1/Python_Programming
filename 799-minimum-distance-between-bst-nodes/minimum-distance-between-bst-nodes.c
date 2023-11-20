afd92ced46e1c028d0d5437fdf5568ed7dc679b2
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2023-11-20 18:05:57 +0530

    Time: 3 ms (48.05%) | Memory: 6.8 MB (44.16%) - LeetSync

---
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.c
@@ -0,0 +1,22 @@
+int min_diff;
+struct TreeNode *prev_node;
+
+void dfs(struct TreeNode *node) {
+    if (!node) return;
+    dfs(node->left);
+
+    if (prev_node) {
+        int curr_diff = node->val - prev_node->val;
+        min_diff = min_diff < curr_diff ? min_diff : curr_diff;
+    }
+    prev_node = node;
+
+    dfs(node->right);
+}
+
+int minDiffInBST(struct TreeNode *root) {
+    prev_node = NULL;
+    min_diff = INT_MAX;
+    dfs(root);
+    return min_diff;
+}
