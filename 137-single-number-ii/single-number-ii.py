8111cbceed12f8556ac72c122ffbeadfb9a59ca3
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-03-11 02:15:07 +0530

    Time: 47 ms (95.04%) | Memory: 18.5 MB (96.85%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,19 +1,10 @@
 class Solution:
-    def singleNumber(self, nums):
-        ans = 0
+  def singleNumber(self, nums: List[int]) -> int:
+    ones = 0
+    twos = 0
 
-        for i in range(32):
-            bit_sum = 0
-            for num in nums:
-                # Convert the number to two's complement representation to handle large test case
-                if num < 0:
-                    num = num & (2**32-1)
-                bit_sum += (num >> i) & 1
-            bit_sum %= 3
-            ans |= bit_sum << i
+    for num in nums:
+      ones ^= (num & ~twos)
+      twos ^= (num & ~ones)
 
-        # Convert the result back to two's complement representation if it's negative to handle large test case
-        if ans >= 2**31:
-            ans -= 2**32
-
-        return ans
+    return ones
