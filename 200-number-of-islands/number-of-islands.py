03888a9759345d4b69e0ba3bee70547f81a05e0c
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-04-06 16:29:00 +0530

    Time: 240 ms (66.90%) | Memory: 24.3 MB (31.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,39 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m=len(grid)
-        n=len(grid[0])
-        parent=[i for i in range(m*n)]
-        rank=[1 for i in range(m*n)]
-        def find(n):
-            if parent[n]!=n:
-                parent[n]=find(parent[n])
-            return parent[n]  
-        def union(x,y):
-            p1=find(x)
-            p2=find(y)
-            if p1==p2:
-                return 
-            else:
-                if rank[p1]<rank[p2]:
-                    parent[p1]=p2
-                    rank[p2]+=rank[p1]
-                else:
-                    parent[p2]=p1
-                    rank[p1]+=rank[p2]
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]=='1':
-                    x1=i*n+j
-                    if(i+1<=m-1 and grid[i+1][j]=='1'):
-                        x2=x1+n
-                        union(x1,x2)
-                    if(j+1<=n-1 and grid[i][j+1]=='1'):    
-                        x2=x1+1
-                        union(x1,x2)
-                else:
-                    rank[i*n+j]=0        
+        if not grid:
+            return 0
+        visit=set()
+        rows,cols=len(grid),len(grid[0])
         ans=0
-        for i in range(m*n):
-            if find(i)==i and rank[i]>0:
-                ans+=1
-        return ans               
+        def bfs(r,c):
+            q=collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            while q:
+                row,col=q.popleft()
+                directions=[[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    r,c=row+dr,col+dc
+                    if(0<=r<rows and 0<=c<cols and
+                        grid[r][c]=="1" and
+                        (r,c) not in visit):
+                        visit.add((r,c))
+                        q.append((r,c))
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and (r,c) not in visit:
+                    bfs(r,c)
+                    ans+=1    
+        return ans            
