905cb1334e8f18a14a5b0bea4fafd96bd1b8dd9b
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-05-21 14:14:19 +0530

    Time: 54 ms (15.24%) | Memory: 17.7 MB (17.50%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -13,4 +13,4 @@ class Solution:
             right=helper(root.right)
             balanced=(left[0] and right[0] and abs(left[1]-right[1])<=1)
             return [balanced,1+max(left[1],right[1])]
-        return helper(root)[0]    
+        return helper(root)[0]        
