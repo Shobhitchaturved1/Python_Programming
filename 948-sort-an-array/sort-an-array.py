af50fce54133fff514c0244ba27029f279ef93ec
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-05-24 16:44:15 +0530

    Time: 835 ms (79.25%) | Memory: 26.8 MB (20.32%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.quickSort(nums)
+
+    def quickSort(self, nums):
+        if len(nums) <= 1:
+            return nums
+
+        pivot = random.choice(nums)
+        lesser, equal, greater = [], [], []
+        for num in nums:
+            if num < pivot:
+                lesser.append(num)
+            elif num == pivot:
+                equal.append(num)
+            else:
+                greater.append(num)
+        
+        return self.quickSort(lesser) + equal + self.quickSort(greater)
+        
