99d9621f23782eebf2337945579cb9bb70707081
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-06-01 22:12:49 +0530

    Time: 424 ms (28.38%) | Memory: 18 MB (17.25%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,16 +1,15 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        ROWS,COLS=len(grid),len(grid[0])
+        rows,cols=len(grid),len(grid[0])
         visit=set()
         ans=0
         directions=[[1,0],[-1,0],[0,1],[0,-1]]
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c]==1 and (r,c) not in visit:           
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==1 and (r,c) not in visit:
                     for dr,dc in directions:
-                        row=r+dr
-                        col=c+dc
-                        if row<0 or row==ROWS or col<0 or col==COLS or grid[row][col]!=1:
+                        row,col=r+dr,c+dc
+                        if row<0 or row==rows or col<0 or col==cols or grid[row][col]!=1:
                             ans+=1
                         visit.add((r,c))
-        return ans                        
+        return ans                    
