ff28cc647c6920de1d6e169be681bad951a05a9a
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-06-01 22:02:36 +0530

    Time: 610 ms (31.35%) | Memory: 21.5 MB (74.27%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,13 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        beingTrustedBy=defaultdict(int)
-        isTrusting=defaultdict(int)
+        beingtrustedby=defaultdict(int)
+        istrusting=defaultdict(int)
         for x,y in trust:
-            isTrusting[x]+=1
-            beingTrustedBy[y]+=1
+            istrusting[x]+=1
+            beingtrustedby[y]+=1
         for i in range(1,n+1):
-            if beingTrustedBy[i]==n-1 and isTrusting[i]==0:
+            if beingtrustedby[i]==n-1 and istrusting[i]==0:
                 return i
         return -1            
-
-          
