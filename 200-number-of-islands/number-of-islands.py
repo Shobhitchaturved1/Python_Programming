5e4af3a2855f846f2dcb80b30c23de4bddfeee78
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-06-03 16:08:09 +0530

    Time: 322 ms (6.27%) | Memory: 23.9 MB (33.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        count=0
         if not grid:return 0
         rows,cols=len(grid),len(grid[0])
         visit=set()
-        ans=0
-        def bfs(r,c):
-            q=collections.deque()
-            q.append((r,c))
+        def dfs(r,c):
+            if (r not in range(rows) or(c not in range(cols)) or grid[r][c]=="0" or (r,c) in visit):
+                return
             visit.add((r,c))
-            while q:
-                row,col=q.popleft()
-                direction=[[1,0],[-1,0],[0,1],[0,-1]]
-                for dr,dc in direction:
-                    r,c=row+dr,col+dc
-                    if(0<=r<rows and 0<=c<cols and grid[r][c]=="1" 
-                        and (r,c) not in visit):
-                        visit.add((r,c))
-                        q.append((r,c))
+            directions=[[1,0],[-1,0],[0,1],[0,-1]]
+            for dr,dc in directions:
+                dfs(r+dr,c+dc)
         for r in range(rows):
             for c in range(cols):
-                if (r,c) not in visit and grid[r][c]=="1":
-                    bfs(r,c)
-                    ans+=1
-        return ans            
+                if grid[r][c]=="1" and (r,c) not in visit:
+                    count+=1
+                    dfs(r,c)            
+        return count                   
