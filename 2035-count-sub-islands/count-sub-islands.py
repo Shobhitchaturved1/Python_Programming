eeffcfdc352bd1a1a51e665bbbdaf86b22b751f1
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-06-03 18:14:45 +0530

    Time: 2427 ms (17.09%) | Memory: 54.3 MB (30.17%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.py
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -1,25 +1,21 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        ROWS,COLS=len(grid1),len(grid1[0])
-        ans=0
+        rows,cols=len(grid1),len(grid1[0])
         visit=set()
+        count=0
         def dfs(r,c):
-            if(r<0 or r==ROWS or c<0 or c==COLS or
-                grid2[r][c]==0 or(r,c) in visit):
-                return True
-            visit.add((r, c))
-            res = True
-            if grid1[r][c] == 0:
-                res = False
-
-            res = dfs(r - 1, c) and res
-            res = dfs(r + 1, c) and res
-            res = dfs(r, c - 1) and res
-            res = dfs(r, c + 1) and res
-            return res   
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (grid1[r][c]==1 and grid2[r][c]==1 and (r,c) not in visit
-                    and dfs(r,c)):
-                    ans+=1
-        return ans            
+            if(r not in range(rows) or
+               c not in range(cols) or
+               (r,c) in visit or
+               grid2[r][c]==0):
+               return 0
+            elif (grid1[r][c]==0 and grid2[r][c]==1):
+                return 1
+            visit.add((r,c))    
+            return 0 + dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1)
+        for r in range(rows):
+            for c in range(cols):
+                if grid2[r][c]==1 and (r,c) not in visit:
+                    if not dfs(r,c):
+                        count+=1
+        return count                
