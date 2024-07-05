8b0074ad7022adea54b99c94c433feb454431c40
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-07-05 14:51:47 +0530

    Time: 83 ms (64.84%) | Memory: 17.4 MB (91.74%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m,n=len(grid),len(grid[0])
+        row=[]
+        a=0
+        for i in reversed(grid[-1]):
+            a+=i
+            row.append(a)
+        row=row[::-1]    
+        for i in reversed(range(m-1)):
+            for j in reversed(range(n)):
+                if j+1<n:
+                    row[j]=grid[i][j]+min(row[j],row[j+1])
+                else:
+                    row[j]+=grid[i][j]    
+            #print(row)        
+        return row[0]            
