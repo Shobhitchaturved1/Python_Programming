df076821036467a1aac3f1cdcde3a74ea9b178a9
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-07-20 18:55:16 +0530

    Time: 41 ms (29.74%) | Memory: 16.5 MB (61.87%) - LeetSync

--- 343-integer-break/integer-break.py
+++ 343-integer-break/integer-break.py
@@ -1,12 +1,9 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         dp={1:1}
-        def dfs(num):
-            if num in dp:
-                return dp[num]
+        for num in range(2,n+1):
             dp[num]=0 if num==n else num
             for i in range(1,num):
-                val=dfs(i)*dfs(num-i) 
-                dp[num]=max(dp[num],val)
-            return dp[num]
-        return dfs(n)          
+                val=dp[i]*dp[num-i]
+                dp[num]=max(val,dp[num])
+        return dp[n]        
