1908ffa712a22be89fc624e1f71336fc7b27095b
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2024-07-27 20:56:27 +0530

    Time: 273 ms (15.49%) | Memory: 83.1 MB (6.89%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        x=0
+        dp=defaultdict(int)
+        def helper(idx,buyorsell,k):
+            if (idx,buyorsell,k) in dp:
+                return dp[(idx,buyorsell,k)]
+            if idx>=len(prices) or k==0:
+                return 0
+            if(buyorsell==0):
+                buy=helper(idx+1,1,k)-prices[idx]
+                nobuy=helper(idx+1,0,k)
+                x=max(buy,nobuy)
+            else:
+                sell=helper(idx+1,0,k-1)+prices[idx]
+                nosell=helper(idx+1,1,k)
+                x=max(sell,nosell)
+            dp[(idx,buyorsell,k)]=x    
+            return x
+        return helper(0,0,k) 
