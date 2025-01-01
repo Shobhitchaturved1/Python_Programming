5a8225d75f80490e909aee60224ed14b49027e16
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2025-01-01 08:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (15.52%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -7,13 +7,12 @@ class Solution:
             if i=='1':
                 count1+=1
         if count1==len(s) or count1==0:
-            return len(s)-1  
-        ans=count1-1         
+            return len(s)-1
+        ans=count1-1
         for i in range(len(s)):
             if s[i]=='0' and i!=len(s)-1:
                 count0+=1
                 ans=max(ans,count0+count1)
             else:
                 count1-=1
-        return ans                   
-        
+        return ans                         
