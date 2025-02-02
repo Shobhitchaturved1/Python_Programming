80a05b10f01d21b11dc7e51e46499ae17c2f94cd
Shobhitchaturved1 <147103730+Shobhitchaturved1@users.noreply.github.com>
2025-02-02 14:58:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.79%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,14 +1,9 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        # Initialize the break variable to 0
         breaks = 0
 
-        # Iterate over the len(nums)
         for i in range(len(nums)):
-            # If you find any break case, increment the break variable
             if nums[i] < nums[i-1]:
                 breaks += 1
         
-        # If there is atmost 1 breaks, return True
-        # Else, return False
         return True if breaks <= 1 else False
