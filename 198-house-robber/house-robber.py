6890badcf97c35224c29ba94bb151ae7f25537fc
Shobhit <147103730+Shobhitchaturved1@users.noreply.github.com>
2025-02-23 00:28:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.06%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,8 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         nums.append(0)
-        if len(nums)==1:return nums[0]
-        nums[-2]=max(nums[-2],nums[-1])
         for i in range(len(nums)-3,-1,-1):
             nums[i]=max(nums[i+1],nums[i]+nums[i+2])
         return max(nums[0],nums[1])    
