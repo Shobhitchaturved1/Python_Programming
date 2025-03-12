e4664127497807db1e024cbe08713213fa7d1402
Shobhit <147103730+Shobhitchaturved1@users.noreply.github.com>
2025-03-12 16:39:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.15%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,33 +1,20 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        # Stacks to store indices of open brackets and asterisks
-        open_brackets = []
-        asterisks = []
-
-        for i, ch in enumerate(s):
-            # If current character is an open bracket, push its index onto the stack
-            if ch == "(":
-                open_brackets.append(i)
-            # If current character is an asterisk, push its index onto the stack
-            elif ch == "*":
-                asterisks.append(i)
-            # current character is a closing bracket ')'
+        stack=[]
+        star=[]
+        for i,x in enumerate(s):
+            if x=="(":
+                stack.append(i)
+            elif x=="*":
+                star.append(i)
             else:
-                # If there are open brackets available, use them to balance the closing bracket
-                if open_brackets:
-                    open_brackets.pop()
-                elif asterisks:
-                    # If no open brackets are available, use an asterisk to balance the closing bracket
-                    asterisks.pop()
+                if stack:
+                    stack.pop()
+                elif star:
+                    star.pop()
                 else:
-                    # nnmatched ')' and no '*' to balance it.
                     return False
-
-        # Check if there are remaining open brackets and asterisks that can balance them
-        while open_brackets and asterisks:
-            # If an open bracket appears after an asterisk, it cannot be balanced, return false
-            if open_brackets.pop() > asterisks.pop():
-                return False  # '*' before '(' which cannot be balanced.
-
-        # If all open brackets are matched and there are no unmatched open brackets left, return true
-        return not open_brackets
+        while stack and star:
+            if stack.pop()>star.pop():
+                return False
+        return not stack        
