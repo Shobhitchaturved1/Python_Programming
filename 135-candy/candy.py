a476bd3550353efb6bdb163339b563396c54e36e
Shobhit <147103730+Shobhitchaturved1@users.noreply.github.com>
2025-03-12 15:45:37 +0530

    Time: 14 ms (73.49%) | Memory: 19.9 MB (70.52%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,17 +1,14 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         ans=[1]*len(ratings)
-        left=1
-        while left<len(ratings):
-            if ratings[left]>ratings[left-1]:
-                ans[left]=ans[left-1]+1
-            left+=1
-        right=len(ratings)-2    
-        while right>=0:
-            if ratings[right]>ratings[right+1]:
-                ans[right]=max(ans[right],ans[right+1]+1)   
-            right-=1
-        #print(ans)    
-        return sum(ans)         
-
-
+        #forward
+        for i in range(1,len(ratings)):
+            if ratings[i]>ratings[i-1]:
+                ans[i]=ans[i-1]+1
+        #backward
+        #print(ans)
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                ans[i]=max(ans[i],ans[i+1]+1)
+        #print(ans)        
+        return sum(ans)        
