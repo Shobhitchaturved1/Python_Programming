03fb62052752df444f96828194d50ad408ae4e3f
Shobhit <147103730+Shobhitchaturved1@users.noreply.github.com>
2025-04-07 18:19:13 +0530

    Time: 198 ms (60.33%) | Memory: 19.5 MB (29.28%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,32 +1,23 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        ROW,COL=len(grid),len(grid[0])
-        if grid[0][0]==1:
+        ROW, COL = len(grid), len(grid[0])
+        if grid[0][0] != 0 or grid[ROW - 1][COL - 1] != 0:
             return -1
-        visit=set()
-        q=deque()
-        q.append([(0,0),1])
-        directions=[[1,0],[-1,0],[0,1],[0,-1]]
-        while q:
-            [row,col],move=q.popleft()
-            if row==ROW-1 and col==COL-1:
-                return move
-            if row+1<ROW and col+1<COL and grid[row+1][col+1]==0 and (row+1,col+1) not in visit:
-                q.append([(row+1,col+1),move+1])  
-                visit.add((row+1,col+1)) 
-            if 0<=row-1 and 0<=col-1 and (row-1,col-1) not in visit and grid[row-1][col-1]==0:
-                q.append([(row-1,col-1),move+1])
-                visit.add((row-1,col-1))   
-            if 0<=row-1 and col+1<COL and (row-1,col+1)not in visit and grid[row-1][col+1]==0:
-                q.append([(row-1,col+1),move+1])
-                visit.add((row-1,col+1))
-            if row+1<ROW and 0<=col-1 and (row+1,col-1) not in visit and grid[row+1][col-1]==0:
-                q.append([(row+1,col-1),move+1])
-                visit.add((row+1,col-1))
-            for dr,dc in directions:
-                r,c=row+dr,col+dc
-                if (r,c) not in visit and 0<=r<ROW and 0<=c<COL and grid[r][c]==0:
-                    q.append([(r,c),move+1])
-                    visit.add((r,c))
-        return -1            
-
+        
+        direction = [[1,0], [-1,0], [0,1], [0,-1], [1,1], [-1,-1], [1,-1], [-1,1]]
+        queue = deque([(0, 0, 1)])  # row, col, path length
+        visited = set((0, 0))
+        
+        while queue:
+            r, c, dist = queue.popleft()
+            if r == ROW - 1 and c == COL - 1:
+                return dist
+            for dr, dc in direction:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < ROW and 0 <= nc < COL and grid[nr][nc] == 0 and (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    queue.append((nr, nc, dist + 1))
+        return -1
